601c93ec1b0f6d1b2d0705adfe849d3530ae3ea9
MarcoBagnasco <bagna.hbk@gmail.com>
2021-06-08 22:50:23 +0200

    add homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,20 @@
 @extends('layouts.main')
 
 @section('content')
-    <h1>DC COMICS</h1>
+    <section class="home-main">
+        <!-- Comics -->
+        <div class="container flex ai-center">
+            <div class="comics flex">
+                @foreach ($comics as $comic)
+                    <div class="card">
+                        <img src="{{ $comic['image'] }}" alt="{{ $comic['title'] }}">
+                        <div class="comic-title">{{ $comic['title'] }}</div>
+                    </div>
+                @endforeach
+            </div>
+
+            <div class="load">load more</div>
+        </div>
+    </section>
 @endsection
 
